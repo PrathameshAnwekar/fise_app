b55e1106dcf2233fe393d2519e45a63af314f7c0
Mr-MeowMeow <satyanarayanprashar73@gmail.com>
2022-07-21 14:07:51 +0530

    basket UI

--- lib/models/user_data.dart
+++ lib/models/user_data.dart
@@ -28,11 +28,15 @@ class UserData {
     var docData = doc.data().toString();
     return UserData(
         email: docData.contains('email') ? doc.get('email') : 'not set',
-        username: docData.contains('username') ? doc.get('username') : 'not set',
-        photoUrl: docData.contains('photoUrl') ? doc.get('photoUrl') : 'not set',
+        username:
+            docData.contains('username') ? doc.get('username') : 'not set',
+        photoUrl:
+            docData.contains('photoUrl') ? doc.get('photoUrl') : 'not set',
         uid: docData.contains('uid') ? doc.get('uid') : 'not set',
         pancard: docData.contains('pancard') ? doc.get('pancard') : 'not set',
-        phoneNumber: docData.contains('phoneNumber') ? doc.get('phoneNumber') : 'not set',
+        phoneNumber: docData.contains('phoneNumber')
+            ? doc.get('phoneNumber')
+            : 'not set',
         aadhar: docData.contains('aadhar') ? doc.get('aadhar') : 'not set',
         address: docData.contains('address') ? doc.get('address') : 'not set',
         dob: docData.contains('dob') ? doc.get('dob') : 'not set',
