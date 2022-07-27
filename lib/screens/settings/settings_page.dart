84959b3449f0aacaf85c7248bf621e3daf1c7ef4
Mr-MeowMeow <satyanarayanprashar73@gmail.com>
2022-07-27 18:34:52 +0530

    minor changes

--- lib/screens/settings/settings_page.dart
+++ lib/screens/settings/settings_page.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fise_app/constants/constants.dart';
-import 'package:fise_app/screens/authentication/gmail_auth.dart';
-import 'package:fise_app/screens/dummy_yfin.dart';
 import 'package:fise_app/screens/settings/app_settings_page.dart';
 import 'package:fise_app/screens/settings/transactions/transactions.dart';
 import 'package:fise_app/screens/settings/user_info/personal_info.dart';
@@ -118,21 +116,6 @@ class SliverCustomHeaderDelegate extends SliverPersistentHeaderDelegate {
               style: TextStyle(
                 fontSize: 35.0,
                 fontWeight: FontWeight.w400,
-              ),
-            ),
-          ),
-          Positioned(
-            left: 0,
-            top: maxExtent / 2,
-            right: 0,
-            bottom: 0,
-            child: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [Color(0x00000000), Color(0x90000000)],
-                ),
               ),
             ),
           ),
