10238a821a2af800cc629b5d9d6c5d71c12a37a4
Prathamesh Anwekar <f20201039@pilani.bits-pilani.ac.in>
2022-08-18 22:27:49 +0530

    fixed basket

--- lib/screens/asset_screens/equity/basket_search.dart
+++ lib/screens/asset_screens/equity/basket_search.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 var stockList;
-var basketMap;
+Map basketMap = {};
 
 class BasketSearchScreen extends ConsumerStatefulWidget {
   BasketSearchScreen({Key? key}) : super(key: key);
@@ -89,9 +89,13 @@ class _BasketSearchScreenState extends ConsumerState<BasketSearchScreen> {
                     color: AppThemeData.lightColorScheme.primary,
                   ),
                   onPressed: () {
-                    basketMap.update(
+                    // basketMap.update(
+                    //     stockDataList[index].companyName,
+                    //     (value) => int.parse(value) + 1,
+                    //     (value) => int.parse(value.toString()) + 1,
+                    //     ifAbsent: () => 1);
+                    ref.read(basketStocksProvider.state).state.update(
                         stockDataList[index].companyName,
-                        (value) => int.parse(value) + 1,
                         (value) => int.parse(value.toString()) + 1,
                         ifAbsent: () => 1);
                     ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
