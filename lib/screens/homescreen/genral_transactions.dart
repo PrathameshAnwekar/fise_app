cc548dc42cb58aec50deff6d3d0cc52e098150fc
Mr-MeowMeow <satyanarayanprashar73@gmail.com>
2022-08-19 21:58:04 +0530

    transaction of home, gold  page

--- lib/screens/homescreen/genral_transactions.dart
+++ lib/screens/homescreen/genral_transactions.dart
@@ -1,12 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../constants/size_config.dart';
+import '../../models/user_data.dart';
 import '../settings/transactions/transaction_tile.dart';
 
-class GeneralTransactions extends StatelessWidget {
+class GeneralTransactions extends ConsumerStatefulWidget {
   const GeneralTransactions({Key? key}) : super(key: key);
 
   @override
+  ConsumerState<GeneralTransactions> createState() =>
+      _GeneralTransactionsState();
+}
+
+class _GeneralTransactionsState extends ConsumerState<GeneralTransactions> {
+  @override
   Widget build(BuildContext context) {
+    var userd = ref.read(currentUserDataProvider.state).state;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -25,38 +36,38 @@ class GeneralTransactions extends StatelessWidget {
             width: SizeConfig.screenWidth * 0.58,
           ),
         ),
-        const TransactionsTile(
-            invested: "16",
-            deductedFrom: "State Bank of India",
-            spent: "32",
-            debitDate: "14 Jun 22"),
-        const TransactionsTile(
-            invested: "16",
-            deductedFrom: "State Bank of India",
-            spent: "32",
-            debitDate: "14 Jun 22"),
-        const TransactionsTile(
-            invested: "16",
-            deductedFrom: "State Bank of India",
-            spent: "32",
-            debitDate: "14 Jun 22"),
-        const TransactionsTile(
-            invested: "16",
-            deductedFrom: "State Bank of India",
-            spent: "32",
-            debitDate: "14 Jun 22"),
-        ListTile(
-          title: const Text(
-            "Invested",
-            style: TextStyle(fontSize: 22),
-          ),
-          trailing: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text("₹ 1000"),
-              Text("10 Jun 22"),
-            ],
-          ),
+        FutureBuilder(
+          future: FirebaseFirestore.instance
+              .collection('transactions')
+              .doc('payments')
+              .collection('${userd!.uid}')
+              .get(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.hasData) {
+              var data = snapshot.data!.docs;
+
+              return Expanded(
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    var docData = data[index].data()! as Map;
+                    return TransactionsTile(
+                      deductedFrom: docData['orderNote'],
+                      debitDate: docData['txTime'] ?? 'Failed',
+                      spent: docData['orderAmount'] ?? '0',
+                      invested: docData['txStatus'] ?? '0',
+                    );
+                  },
+                ),
+              );
+            } else {
+              return Text("You have no transactions yet.");
+            }
+          },
         ),
       ],
     );
